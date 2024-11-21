5f071f695f0019a34254e8ea71f510a6c5dd9bf9
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2024-11-21 11:03:39 +0000

    Add an outputs preview
    Signed-off-by: Ilya Savitsky <ilya.savitsky@codethink.co.uk>

--- explore_tf_state.sh
+++ explore_tf_state.sh
@@ -1,3 +1,20 @@
+choice=$(dialog --clear \
+    --menu "What part of terraform state do you want to browse?" 0 0 0 \
+    "state" "List terraform state" \
+    "output" "List terraform outputs" \
+    2>&1 >/dev/tty)
+
+case $choice in
+"state")
+    bind="start:reload:tofu state list"
+    preview="tofu state show {1} | bat --style=plain --color=always -l hcl"
+    ;;
+"output")
+    bind="start:reload:tofu output -json | jq -r 'keys[]'"
+    preview="tofu output {1} | bat --style=plain --color=always -l json"
+    ;;
+esac
+
 fzf --ansi \
-    --bind "start:reload:tofu state list" \
-    --preview "tofu state show {1} | bat --style=plain --color=always -l hcl"
+    --bind "$bind" \
+    --preview "$preview"
