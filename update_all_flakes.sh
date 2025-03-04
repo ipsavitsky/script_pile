e5b8035817e1731baef10c1baca9fd9a77136ac6
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2025-03-04 12:24:58 +0000

    Update the update_all_flakes script

--- update_all_flakes.sh
+++ update_all_flakes.sh
@@ -3,8 +3,35 @@ for fl in $(fd flake.nix .); do
     printf "parsing: %s\n" "$wdir"
     if gum confirm "Proceed with $wdir?"; then
         cd "$wdir" || exit
+        if [ -n "$(git status --porcelain)" ]; then
+            printf "There are uncommited changes in %s\n" "$wdir"
+            continue
+        fi
+
+        if [ -f "Cargo.toml" ]; then
+            if gum confirm "This looks like a Rust project. Update Rust dependencies?"; then
+                cargo update
+            fi
+        fi
+
+        if [ -f "go.mod" ]; then
+            if gum confirm "This looks like a go project. Update go dependencies?"; then
+                go get -u
+                go mod tidy
+            fi
+
+            if gum confirm "Do you maintain a scripts/vendor_hash.sh? Run it to update vendor hash?"; then
+               ./scripts/vendor_hash --update
+            fi
+        fi
+
         nix flake update
+        if gum confirm "Commit and push $wdir?"; then
+            git commit -am "Update"
+            git push
+        fi
+
         cd ..
-        printf "%s updated, don't forget to check build and commit\n" "$wdir"
+        printf "%s updated\n" "$wdir"
     fi
 done
