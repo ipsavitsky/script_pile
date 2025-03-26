62555873fe6052f76d95ed48eebb86df3032b8bb
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2025-03-26 17:01:15 +0000

    Add statix/deadnix to treefmt

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -1,7 +1,9 @@
-{ ... }:
+_:
 {
   projectRootFile = "flake.nix";
   programs.nixfmt.enable = true;
+  programs.statix.enable = true;
+  programs.deadnix.enable = true;
   programs.shfmt.enable = true;
   programs.shellcheck.enable = true;
   programs.taplo.enable = true;
