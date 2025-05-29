8dec893edb15ede63bd4c32c769fc636ca98767f
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2025-05-29 09:49:43 +0100

    Remove ploc; format

--- flake.nix
+++ flake.nix
@@ -2,24 +2,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    pyproject-nix = {
-      url = "github:pyproject-nix/pyproject.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    uv2nix = {
-      url = "github:pyproject-nix/uv2nix";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    pyproject-build-systems = {
-      url = "github:pyproject-nix/build-system-pkgs";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.uv2nix.follows = "uv2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -32,25 +14,19 @@
       nixpkgs,
       flake-utils,
       treefmt-nix,
-      uv2nix,
-      pyproject-nix,
-      pyproject-build-systems,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
         treefmtModule = treefmt-nix.lib.evalModule pkgs ./nix/treefmt.nix;
-        workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./ploc; };
-        overlay = workspace.mkPyprojectOverlay { sourcePreference = "wheel"; };
-        pythonSet = (pkgs.callPackage pyproject-nix.build.packages {
-          python = pkgs.python312;
-        }).overrideScope (
-          pkgs.lib.composeManyExtensions [
-            pyproject-build-systems.overlays.default
-            overlay
-          ]
-        );
+        runtimePackages = with pkgs; [
+          gum
+          fzf
+          opentofu
+          bat
+          jq
+        ];
       in
       {
         formatter = treefmtModule.config.build.wrapper;
@@ -60,25 +36,18 @@
         };
 
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ uv python3 ];
+          packages = runtimePackages;
         };
 
-        packages = {
+        packages = rec {
+          default = explore_tf_state;
           explore_tf_state = pkgs.writeShellApplication {
             name = "explore_tf_state";
 
-            runtimeInputs = with pkgs; [
-              gum
-              fzf
-              opentofu
-              bat
-              jq
-            ];
+            runtimeInputs = runtimePackages;
 
             text = builtins.readFile ./explore_tf_state/explore_tf_state.sh;
           };
-
-          ploc = pythonSet.mkVirtualEnv "ploc" workspace.deps.default;
         };
       }
     );
--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -5,8 +5,4 @@ _: {
   programs.deadnix.enable = true;
   programs.shfmt.enable = true;
   programs.shellcheck.enable = true;
-  programs.taplo.enable = true;
-  programs.isort.enable = true;
-  programs.ruff-format.enable = true;
-  programs.ruff-check.enable = true;
 }
